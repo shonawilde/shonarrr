e888779075f250849f85a3a4b7bbbfc527c77a2b
Shona Wilde <55277552+shonawilde@users.noreply.github.com>
2019-09-16 15:00:14 +0100

    update function to deal with data from different sites

--- R/read_ceda_met_file.R
+++ R/read_ceda_met_file.R
@@ -4,48 +4,62 @@
 #'
 #' @param file path to file
 #' 
+#' @param site "KM" or "LP"
+#' 
 #' @author Shona Wilde
 #' 
 #' @return data frame
 #' 
 #' @export
 
-read_ceda_met_file <- function(file) {
-  
+read_ceda_met_file <- function(file, site){
   
   # read lines
-  text <- readr::read_lines(file)
+  text <- readr::read_lines(file) %>% 
+    str_to_lower()
   
   # isolate preamble
-  df_preamble <- text[2:(which(text %in% "4-bad data"))]
+  df_preamble <- text[2:(str_which(text, "bad"))]
   
   # get date
-  date <- text[str_which(text, "Time in minutes")] %>% 
+  date <- text[str_which(text, "time in minutes")] %>% 
     str_split_fixed("since", 2) %>% 
     nth(2) %>% 
     str_remove(" ") %>% 
     ymd_hm()
   
   # get variable names
-  variable_names <- text[str_which(text, "Time in minutes"): str_which(text, "4-bad data") + 4L] %>% 
+  variable_names <- text[str_which(text, "time in minutes"): str_which(text, "bad") + 4L] %>% 
+    str_rm_round_brackets()  %>% 
+    str_trim() %>% 
     str_to_lower() %>% 
-    str_remove_all(" ") %>% 
-    str_replace("winddirection", "wd") %>% 
-    str_replace("windspeed", "ws") %>%
-    str_replace("airpressure", "air_pressure") %>% 
-    str_replace("temperature", "air_temp") %>% 
-    str_rm_round_brackets()
+    str_replace_all(" ", "_") %>% 
+    str_replace("wind_direction", "wd") %>% 
+    str_replace("wind_speed", "ws") %>%
+    str_replace("windspeed", "ws") %>% 
+  #  str_replace("air_pressure", "air_pressure") %>% 
+    str_replace("temperature", "air_temp")
   
   # drop extra variable names
-  drop_vals = (length(variable_names)-8):length(variable_names)
-  variable_names <- c("mins", variable_names[-drop_vals])
+  if (site %in% "LP"){
+    
+    drop_vals = (length(variable_names)-9):length(variable_names)
+    variable_names <- c("mins", variable_names[-drop_vals])
+  }
+  
+  else {
+    drop_vals = (length(variable_names)-8):length(variable_names)
+    variable_names <- c("mins", variable_names[-drop_vals])
+    
+  }
   
   # read tablular data
-  df <- text[(stringr::str_which(text, "4-bad data") + 4L):length(text)] %>% 
+  df <- text[(stringr::str_which(text, "bad") + 4L):length(text)] %>% 
     read_table2(col_names = F) %>% 
     purrr::set_names(variable_names) %>% 
     mutate(date = date + mins) %>% 
     select(date, everything(), -mins)
   
   return(df)
+  
 }
