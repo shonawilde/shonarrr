51f17cd9121f8300372409f7f971529a6cdccbce
Shona Wilde <55277552+shonawilde@users.noreply.github.com>
2019-09-17 10:40:50 +0100

    add ggplot theme functions for plotting

---
+++ R/theme_shona_less_minimal.R
@@ -0,0 +1,37 @@
+#' Theme Less Minimal Big
+#'
+#' theme_minimal but bigger and less minimal
+#' 
+#' @author Shona Wilde
+#' 
+#' @export
+#' 
+
+
+
+theme_shona_less_minimal <- function() {
+  theme_minimal() +
+    theme(plot.background = element_blank())+
+    theme(plot.margin = margin(1, 1, 1, 1, "cm"),
+          axis.line = element_line()) +                     # facet border
+    theme(strip.background= element_rect(fill = NA),
+          panel.background = element_rect(fill = NA),
+          axis.ticks = element_line()) +               # facet title background
+    theme(text = element_text(colour = "gray13", size = 15)) +
+    theme(axis.text = element_text(colour = "gray13", size = 15)) +
+    theme(plot.title = element_text(colour = "gray13")) +
+    theme(strip.text.x = element_text(colour = "gray13", face = "bold")) +
+    theme(axis.title.y = element_text(size = 20,
+                                      margin = margin(t = 0, r = 20, b = 0, l = 0))) +
+    theme(axis.title.x = element_text(size = 20,
+                                      margin = margin(t = 20, r = 0, b = 0, l = 0))) +
+    theme(panel.spacing = unit(1, "lines")) +
+    theme(panel.grid.major.x = element_blank()) +
+    theme(legend.background = element_blank()) +
+    theme(legend.text = element_text(size = 20)) +
+    theme(legend.title = element_text(size = 20))
+  # theme(legend.key.size = unit(3, "point")) 
+  
+  
+}
+
---
+++ R/theme_shona_white_big.R
@@ -0,0 +1,32 @@
+#' Theme Minimal Big
+#'
+#' theme_minimal but bigger
+#' 
+#' @author Shona Wilde
+#' 
+#' @export
+
+theme_shona_white_big <- function() {
+  theme_minimal() +
+    theme(panel.background = element_blank()) +
+    theme(plot.background = element_blank())+
+    theme(plot.margin = margin(1, 1, 1, 1, "cm"),
+          axis.line = element_line()) +                     # facet border
+    theme(strip.background = element_blank()) +               # facet title background
+    theme(text = element_text(colour = "gray13", size = 15)) +
+    theme(axis.text = element_text(colour = "gray13", size = 15)) +
+    theme(plot.title = element_text(colour = "gray13")) +
+    theme(strip.text.x = element_text(colour = "gray13", face = "bold")) +
+    theme(axis.title.y = element_text(size = 20,
+                                      margin = margin(t = 0, r = 20, b = 0, l = 0))) +
+    theme(axis.title.x = element_text(size = 20,
+                                      margin = margin(t = 20, r = 0, b = 0, l = 0))) +
+    theme(panel.spacing = unit(1, "lines")) +
+    theme(panel.grid.major.x = element_blank()) +
+    theme(legend.background = element_blank()) +
+    theme(legend.text = element_text(size = 20)) +
+    theme(legend.title = element_text(size = 20))
+   # theme(legend.key.size = unit(3, "point")) 
+
+  
+}
