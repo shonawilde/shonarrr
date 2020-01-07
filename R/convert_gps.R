d7b00bc92047c54412de95a8f6af5e0f1e29b3b9
Shona Wilde <55277552+shonawilde@users.noreply.github.com>
2020-01-07 15:15:47 +0000

    Update convert_gps.R
    takes into account directional component of cooridnates

--- R/convert_gps.R
+++ R/convert_gps.R
@@ -2,7 +2,9 @@
 #'
 #' Convert GPS data from WACL van to decimal degrees
 #'
-#' @param coord lat/long
+#' @param coord latitude or longitude column to be converted
+#' 
+#' @param direction column containing directional component of coordinates (N, S, E, W)
 #' 
 #' @author Shona Wilde
 #' 
@@ -12,12 +14,21 @@
 
 
 # function to convert coordinates
-conv_gps <- function(coord) {
+conv_gps <- function(coord, direction) {
   
-  int = floor(coord/100)
+  # is directional component negative
+  is_negative <- ifelse(direction %in% c("S", "W"), TRUE, FALSE)
   
-  new_coord = (((coord/100 - int)*100)/60) + int
+  int <- floor(coord/100)
+  
+  new_coord <- (((coord/100 - int)*100)/60) + int
+  
+  suppressWarnings(if (is_negative)
+    
+    new_coord <- ((((coord/100 - int)*100)/60) + int) *-1)
+  
   
   return(new_coord)
   
 }
+
