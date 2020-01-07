77a5081fe51c44fc66e4c0cfeaa355d2c04558a7
Shona Wilde <55277552+shonawilde@users.noreply.github.com>
2020-01-07 18:07:19 +0000

    Create read_syft_data.R
    new function

---
+++ R/read_syft_data.R
@@ -0,0 +1,34 @@
+#' Read SYFT data
+#'
+#' Reads worked up SYFT data 
+#'
+#' @param file file
+#' 
+#' @author Shona Wilde
+#' 
+#' @return tibble
+#' 
+#' @export
+
+read_syft_data <- function(file) {
+  
+  df <- read_xlsx(file) %>% 
+    rename_all(. %>% tolower()) %>% 
+    rename_all(. %>% str_rm_round_brackets()) %>% 
+    rename_all(. %>% str_replace_all(" ", "")) %>% 
+    rename_all(. %>% str_remove("corr")) %>%
+    rename_all(. %>% str_replace("-", "_")) %>% 
+    rename(date = actualtime) %>% 
+    mutate(date = floor_date(date, "secs")) %>%
+    select(date, everything()) %>% 
+    as_tibble()
+  
+  if (suppressWarnings(!class(df$date)[1] %in% "POSIXct"))
+    
+    df$date <- ymd_hms(df$date)
+  
+  return(df)
+  
+}
+
+
