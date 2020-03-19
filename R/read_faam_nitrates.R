ce69a10c167b3cfe18f540718923fab5701ce034
Shona Wilde <55277552+shonawilde@users.noreply.github.com>
2020-03-19 09:51:51 +0000

    update nitrate reader to deal with 1hz and 10hz data structure

--- R/read_faam_nitrates.R
+++ R/read_faam_nitrates.R
@@ -1,8 +1,9 @@
-#' Read  FAAM core nitrates file
+
+#' Read FAAM core nitrates file
 #'
 #' Reads and cleans core nitrates FAAM file
 #'
-#' @param nitrates_file_list List containing core nitrates file names
+#' @param nitrates_file_list List containing file names
 #' 
 #' @author Shona Wilde
 #' 
@@ -17,59 +18,77 @@ read_core_nitrates_data <- function(nitrates_file_list, verbose = TRUE) {
           verbose = verbose)
 }
 
-
-
 read_core_nitrates_worker <- function(file, verbose) {
-  
-  # message
+ 
+   # message
   if (verbose) message(threadr::date_message(), "`", basename(file), "`...")
   
-# open connection
+  # Connection to ncdf file
   nc <- ncdf4::nc_open(file)
   
-  # get meta data from ncdf
-  meta_data <- nc %>% 
-    capture.output()  
+  # Get and format dates
+  date_start <- nc$dim$time$units %>% 
+    stringr::str_remove("seconds since ") %>% 
+    lubridate::ymd_hms(tz = "UTC")
   
-  # close connection
-  ncdf4::nc_close(nc)
+  date <- date_start + as.vector(nc$dim$time$vals) 
   
-  # pull flight number and date
-  flight_no <- meta_data[str_which(meta_data, "flight_number")] %>% 
-    str_split(":", simplify = F)  %>% 
-    map_chr(`[` (2)) %>% 
-    str_remove(" ") %>% 
-    str_to_lower()
+  flight_no = str_sub(file, start = -7, end = -4)
   
-  flight_date <- meta_data[str_which(meta_data, "flight_date")] %>% 
-    str_split(":", simplify = F)  %>% 
-    map_chr(`[` (2)) %>% 
-    str_remove(" ") %>% 
-    paste0(" 00:00") %>% 
-    lubridate::ymd_hm()
+  # Get variable names
+  variables <- names(nc$var)
   
+  # Extract all data
+  list_nc <- variables %>% 
+    purrr::set_names(variables) %>% 
+    purrr::map(~ncdf4::ncvar_get(nc, varid = .))
   
-  # read tidy nc
-  nc_tidy <- tidync::tidync(file)
+  class_list_nc <- list_nc %>% 
+    purrr::set_names(variables) 
   
+  # Function to change colnames
+  change_colname_value <- function(df, new_colname){
+    df %>% rename(!!new_colname := value)
+  }
   
-  # read grid with core data
-  data_core <- tidync(file) %>% 
-    activate("D0") %>% 
-    hyper_tibble()
+  # extract all 10 Hz matrix elements
+  nc_matrix <- list_nc %>% 
+    keep(is.matrix)
   
-  # read grid with mixing ratios
-  data_conc <- tidync(file) %>%
-    activate("D1,D0") %>% 
-    hyper_tibble()
+  # get matrix names
+  names_matrix <- names(nc_matrix)
+
+# clean and bind all 10 Hz data  
+  data_10hz <- nc_matrix %>% 
+    map(reshape2::melt) %>% 
+    map(as_tibble) %>% 
+    map2(names_matrix, change_colname_value) %>% 
+    map(select, -1:-2) %>%  
+    reduce(bind_cols) %>% 
+    mutate(date = rep(date, each = 10)) 
+  # extract all 1 Hz elements
+  nc_array <- list_nc %>% 
+    discard(is.matrix)
   
-  # merge
-  data_all <- data_core %>% 
-    left_join(data_conc, "time") %>% 
-    mutate(date = flight_date + time,
-           flight_no = flight_no) %>%  
-    select(date, flight_no, everything(), -time)
+  # get array names
+  names_array <- names(nc_array)
   
+  # clean and bind 1 Hz data
+  data_1hz <- nc_array %>% 
+    map(as_tibble) %>% 
+    map(mutate, date = date) %>% 
+    map2(names_array, change_colname_value) %>% 
+    reduce(left_join, "date") 
+    
+  
+  # merge all data
+  data_all <- data_10hz %>% 
+    left_join(data_1hz, "date") %>% 
+    mutate(flight_no = flight_no) %>% 
+    select(date, flight_no, everything())
+  
+  return(data_all)
+
 }
 
 
