e56907c22b327d7c3fc43fc432494c713c76af2d
Shona Wilde <shona.wilde@york.ac.uk>
2021-08-05 16:33:21 +0200

    add string functions

--- R/str_extract_round_brackets.R
+++ R/str_extract_round_brackets.R
@@ -1,12 +1,10 @@
-#' Extract text within round brackets
-#'
-#' Extracts texts contained within round brackets
+#' Functions for string manipulation
 #' 
-#' @param x Input string
+#' @param x Input string.
 #' 
-#' @author Shona Wilde
+#' @author Shona Wilde.
 #' 
-#' @return Output string
+#' @return Output string.
 #' 
 #' @export
 
@@ -17,4 +15,26 @@ str_extract_round_brackets <- function(x) {
     stringr::str_sub(2L, -2L)
   
 }
+
+
+#' @rdname str_extract_round_brackets
+#' @export
+str_extract_square_brackets <- function (x) 
+{
+  stringr::str_extract(x, "\\[[^()]+\\]") %>% stringr::str_sub(2L, -2L)
+}
+
+
+
+
+#' @rdname str_extract_round_brackets
+#' @export
+str_rm_unicode <- function (x)
+  
+{
+  stringr::str_replace_all(x, "[^\u0001-\u007F]+|<U\\+\\w+>", "")
+}
+
+
+
 
